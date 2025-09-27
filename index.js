c96cab0a32ff5cf3895587749c7575a424542a23
Gouri saini <gourisaini241@gmail.com>
2025-09-27 15:23:30 +0530

    for prof

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       httpOnly: true,
       sameSite: "none",
     },
